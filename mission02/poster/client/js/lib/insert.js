26a284545bceab943962da1f9357b4dcd7f70bf8
jjang-aaa <jadoit473@gmail.com>
2023-07-23 17:43:21 +0900

    mission02 real complete

---
+++ mission02/poster/client/js/lib/insert.js
@@ -0,0 +1,47 @@
+// beforebegin => insertBefore
+function insertBefore(node, text) {
+  if (typeof node === "string") node = getNode(node);
+
+  if (node.nodeType !== document.ELEMENT_NODE) {
+    throw new ReferenceError(
+      "insertBefore 함수의 첫 번째 인수는 ELEMENT NODE 이어야 합니다."
+    );
+  }
+  node.insertAdjacentHTML("beforebegin", text);
+}
+
+// afterbegin  => insertFirst
+function insertFirst(node, text) {
+  if (typeof node === "string") node = getNode(node);
+
+  if (node.nodeType !== document.ELEMENT_NODE) {
+    throw new ReferenceError(
+      "insertFirst 함수의 첫 번째 인수는 ELEMENT NODE 이어야 합니다."
+    );
+  }
+  node.insertAdjacentHTML("afterbegin", text);
+}
+
+// beforeend   => insertLast
+function insertLast(node, text) {
+  if (typeof node === "string") node = getNode(node);
+
+  if (node.nodeType !== document.ELEMENT_NODE) {
+    throw new ReferenceError(
+      "insertLast 함수의 첫 번째 인수는 ELEMENT NODE 이어야 합니다."
+    );
+  }
+  node.insertAdjacentHTML("beforeend", text);
+}
+
+// afterend    => insertAfter
+function insertAfter(node, text) {
+  if (typeof node === "string") node = getNode(node);
+
+  if (node.nodeType !== document.ELEMENT_NODE) {
+    throw new ReferenceError(
+      "insertAfter 함수의 첫 번째 인수는 ELEMENT NODE 이어야 합니다."
+    );
+  }
+  node.insertAdjacentHTML("afterend", text);
+}
---
+++ mission02/poster/client/js/main.js
@@ -0,0 +1,94 @@
+const body = getNode("body");
+const nav = getNode(".nav");
+
+// 노래 루프
+const visualImage = getNode(".visual img");
+
+const elementalOST = new AudioPlayer("./assets/audio/elementalOST.mp3");
+
+// 이미지 클릭시 노래 토글
+visualImage.addEventListener("click", toggleOST);
+
+let isStartOST = false;
+function toggleOST() {
+  if (isStartOST === true) {
+    elementalOST.pause();
+    isStartOST = false;
+  } else {
+    isStartOST = true;
+    elementalOST.play();
+  }
+}
+
+function showNextPoster(e) {
+  e.preventDefault();
+
+  const nickName = getNode(".nickName");
+  const imgList = getNodes(".nav ul li");
+  const visualImage = getNode(".visual img");
+
+  // 클릭된 이미지의 제일 근처에 있는 Li들고오기
+  const target = e.target.closest("li");
+  if (!target) return;
+
+  // is-active 재설정
+  setIsActive(imgList, target);
+
+  const index = attr(target, "data-index");
+
+  // nav 클릭시 배경 색상 변경
+  setBgColor(index);
+
+  // 이미지 변경
+  setImage(visualImage, index);
+
+  // 텍스트 변경
+  setNameText(nickName, index);
+
+  // 오디오 변경
+  setAudio(index);
+}
+
+// 클릭 이벤트 활성화
+nav.addEventListener("click", showNextPoster);
+
+// 함수 분리
+function setIsActive(imgList, target) {
+  imgList.forEach((li) => removeClass(li, "is-active"));
+  addClass(target, "is-active");
+}
+
+function setBgColor(index) {
+  body.style.background = `linear-gradient(to bottom,${
+    data[index - 1].color
+  }, #000000)`;
+}
+
+function setImage(visualImage, index) {
+  attr(
+    visualImage,
+    "src",
+    `./assets/${data[index - 1].name.toLowerCase()}.jpeg`
+  );
+}
+
+function setNameText(nickName, index) {
+  nickName.textContent = data[index - 1].name;
+}
+
+const audioList = [
+  new AudioPlayer(`./assets/audio/${data[0].name.toLowerCase()}.m4a`),
+  new AudioPlayer(`./assets/audio/${data[1].name.toLowerCase()}.m4a`),
+  new AudioPlayer(`./assets/audio/${data[2].name.toLowerCase()}.m4a`),
+  new AudioPlayer(`./assets/audio/${data[3].name.toLowerCase()}.m4a`),
+];
+
+function setAudio(index) {
+  audioList.forEach((soundPlayer) => soundPlayer.stop());
+
+  const audioPlayer = audioList[index - 1];
+  if (index == 4 || index == 2) {
+    audioPlayer.setVolume(0.15);
+  }
+  audioPlayer.play();
+}
